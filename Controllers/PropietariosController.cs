023283a060950bd11440dff2e5952802fc7d5140
Diegom53 <diegomdiaz53@gmail.com>
2023-05-12 14:19:48 -0300

    12/05/2023 14:20
    Agregados los metodos para crear el token (login) y obtener al propietario logueado mediante su token

--- Controllers/PropietariosController.cs
+++ Controllers/PropietariosController.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Api_Inmobiliaria.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api_Inmobiliaria.Controllers;
 
@@ -8,19 +14,98 @@ namespace Api_Inmobiliaria.Controllers;
 public class PropietariosController : ControllerBase
 {
     private readonly DataContext _context;
-    public PropietariosController(DataContext context)
+    private readonly IConfiguration config;
+    public PropietariosController(DataContext context, IConfiguration config)
     {
         _context = context;
+        this.config = config;
     }
 
-    [HttpGet("{id}")] // devuelve un propietario
-    public Propietario Get(int id)
+    // GET: propietarios/
+    [HttpGet("user")]
+    [Authorize]
+    public async Task<ActionResult<Propietario>> GetUser() // devuelve el propietario logueado
     {
-        return _context.Propietarios.Find(id);
+        try
+        {
+            var usuario = User.Identity.Name;
+            if(usuario == null) return Unauthorized("Token no válido");
+            var dbUser = await _context.Propietarios.SingleOrDefaultAsync(x => x.Correo == usuario);
+            if(dbUser == null) return BadRequest("El usuario no existe");
+            return dbUser;
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    } 
+
+    // GET: propietarios/:id
+    [HttpGet("{id}")] // devuelve un propietario
+    public ActionResult<Propietario> Get(int id)
+    {
+        var propietario = _context.Propietarios.Find(id);
+        if(propietario == null) return NotFound();
+        return propietario;
     }
 
     [HttpGet("test")] // devuelve todos los propietarios
-    public IEnumerable<Propietario> Get(){
-        return _context.Propietarios;
+    public async Task<IActionResult> Get(){
+        try{
+            return Ok(await _context.Propietarios.ToListAsync());
+        }catch(Exception e){
+            return BadRequest(e.Message);
+        }
     }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromForm] LoginView loginView)
+    {
+        try
+        {
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: loginView.Clave,
+                salt: System.Text.Encoding.ASCII.GetBytes(config["Salt"]),
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8
+            ));
+            Console.WriteLine(hashed);
+            var p = await _context.Propietarios.FirstOrDefaultAsync( x => x.Correo == loginView.Correo);
+            if(p == null || p.Clave != hashed)
+            {
+                return BadRequest("Nombre de usuario o clave incorrectos");
+            }
+            else
+            {
+                var key = new SymmetricSecurityKey(
+                    System.Text.Encoding.ASCII.GetBytes(
+                        config["TokenAuthentication:SecretKey"]
+                    )
+                );
+                var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, p.Correo),
+                    new Claim("FullName", p.Nombre + " " + p.Apellido),
+                    new Claim(ClaimTypes.Role, "Propietario")
+                };
+
+                var token = new JwtSecurityToken(
+                    issuer: config["TokenAuthentication:Issuer"],
+                    audience: config["TokenAuthentication:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: credenciales
+                );
+
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            }
+        }
+        catch(Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
+using System.Security.Claims;
 using Api_Inmobiliaria.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddAuthentication().AddJwtBearer(options => // web api valida con token
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["TokenAuthentication:Issuer"],
+        ValidAudience = configuration["TokenAuthentication:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(
+            configuration["TokenAuthentication:SecretKey"]
+        ))
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if(!string.IsNullOrEmpty(accessToken))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Propietario", policy => { policy.RequireClaim(ClaimTypes.Role, "Propietario");});
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +65,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
